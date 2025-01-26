99a5b6dddbfdb01448480826a16d6c5da7073e75
Ali Abouelazz <ali.abouelazz@gmail.com>
2025-01-26 23:57:51 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 
-import MainPage from './pages/mainPage.tsx'
-import EducationExperience from './pages/experience.tsx'
-import DownloadsPage from './pages/downloadsPage.tsx'
-import CompetencesPage from './pages/competencesPage.tsx'
-import CentreInteret from './pages/loisirs.tsx'
-import { HashRouter as Router,Routes,Route} from 'react-router-dom'
-
-
 function App() {
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/educationExperience' element={<EducationExperience/>}/>
-        <Route path="/home" element={<MainPage/>} />
-        <Route path='/downloadsPage' element={<DownloadsPage/>}/>
-        <Route path='/competencesPage' element={<CompetencesPage/>}/>
-        <Route path='/loisirs' element={<CentreInteret/>}/>
-      </Routes>
-    </Router>
+    <>
+      <p>PLEASE WORK</p>
+    </>
   )
 }
 
